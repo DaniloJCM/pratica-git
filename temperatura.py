656623314e9789cfa80e6943cf3e324eb954052f
DaniloJCM <danilojcm04@gmail.com>
2026-05-25 18:38:58 -0400

    insercao de prints na temperatura.py

--- temperatura.py
+++ temperatura.py
@@ -7,3 +7,11 @@ class conversor_de_temperatura:
 
     def fahrenheit_para_celsius(self, fahrenheit):
         return (fahrenheit - 32) * 5 / 9
+
+celsius = 0
+print(f"Celsius {celsius} para Fahrenheit:")
+print(conversor_de_temperatura().celsius_para_fahrenheit(celsius))  # Deve imprimir 32.0
+
+fahrenheit = 32
+print(f"Fahrenheit {fahrenheit} para Celsius:")
+print(conversor_de_temperatura().fahrenheit_para_celsius(fahrenheit))  # Deve imprimir 0.0
